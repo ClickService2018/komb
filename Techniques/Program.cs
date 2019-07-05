0b6c187f11d3019b50a984c602a9f61af58ef15b
ClickService2018 <clickservice2018@github.com>
2019-07-05 15:08:20 +0200

    import project

---
+++ Techniques/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Techniques
+{
+    public static class Program
+    {
+        private static bool NextCombination(IList<int> num, int n, int k)
+        {
+            bool finished;
+
+            var changed = finished = false;
+
+            if (k <= 0) return false;
+
+            for (var i = k - 1; !finished && !changed; i--)
+            {
+                if (num[i] < n - 1 - (k - 1) + i)
+                {
+                    num[i]++;
+
+                    if (i < k - 1)
+                        for (var j = i + 1; j < k; j++)
+                            num[j] = num[j - 1] + 1;
+                    changed = true;
+                }
+                finished = i == 0;
+            }
+
+            return changed;
+        }
+
+        private static IEnumerable Combinations<T>(IEnumerable<T> elements, int k)
+        {
+            var elem = elements.ToArray();
+            var size = elem.Length;
+
+            if (k > size) yield break;
+
+            var numbers = new int[k];
+
+            for (var i = 0; i < k; i++)
+                numbers[i] = i;
+
+            do
+            {
+                yield return numbers.Select(n => elem[n]);
+            } while (NextCombination(numbers, size, k));
+        }
+
+
+        private static void Main()
+        {
+            const int k = 13;
+            var n = new[] { "1", "1", "1", "1", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "1", "1", "1", "1", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "1", "1", "1", "1", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+
+            Console.Write("n: ");
+            foreach (var item in n)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("k: {0}", k);
+            Console.WriteLine();
+
+            for (int j = 1; j < n.Length; j++)
+            {
+                foreach (IEnumerable<string> i in Combinations(n, j))
+                    Console.WriteLine(string.Join(" ", i));
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ po/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace po
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-D5MISNE\SQLEXPRESS;Initial Catalog=FXvendorSCAN;Persist Security Info=True;User ID=sa;Password=123halo321");
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT top 3 [extid],[anzahl] FROM [FXvendorSCAN].[dbo].[tm140916FXamVCorderLOG]", connection);
+            SqlDataReader dataReader = command.ExecuteReader();
+            List<string> nae = new List<string>();
+            int count = 0;
+            while (dataReader.Read())
+            {
+                for (int j = 0; j < Convert.ToInt32(dataReader[1].ToString()); j++)
+                {
+                    nae.Add(dataReader[0].ToString());
+                }
+               // count++;
+                //Console.WriteLine(dataReader[0] + " - " + dataReader[1]);
+               // nae.Add(dataReader[0].ToString());
+            }
+            //for (int i = 0; i < count; i++)
+            //{
+            //    for (int j = 0; j < Convert.ToInt32(dataReader[1].ToString()); j++)
+            //    {
+            //        nae.Add(dataReader[0].ToString());
+            //    }
+            //}
+            string[,] ean = new string[,] { { "a", "2" }, { "b", "5" }, { "c", "3" }, { "d", "2" } };
+            List<string> allEan = new List<string>();
+            List<string> combination = new List<string>();
+            List<string> final = new List<string>();
+
+
+            // Preapre ean list
+            for (int i = 0; i < ean.Length / 2; i++)
+            {
+                for (int j = 0; j < Convert.ToInt32(ean[i, 1]); j++)
+                {
+                    allEan.Add(ean[i, 0]);
+                }
+            }
+
+
+
+            combinations(nae, combination);
+
+            foreach (string comb in combination)
+            {
+                if (final.Count > 0)
+                {
+                    //var aSet = new HashSet<char>(comb);
+                    int combCount = final.Count;
+                    bool find = false;
+                    for (int i = 0; i < combCount; i++)
+                    {
+                        //bool abSame = aSet.SetEquals(final[i]);
+                        if (final[i] == comb)
+                        {
+                            find = true;
+                            break;
+                        }
+                    }
+                    if (find == false)
+                    {
+                        final.Add(comb);
+                    }
+                }
+                else if (final.Count == 0)
+                {
+                    final.Add(comb);
+                }
+
+                foreach (string result in final)
+                {
+                    if (result[0] == 'd')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine(result);
+                    }
+                    if (result[0] == 'c')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(result);
+                    }
+                    if (result[0] == 'b')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(result);
+                    }
+                    if (result[0] == 'a')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine(result);
+                    }
+
+                }
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("All Combination: " + combination.Count);
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Final Combinations: " + final.Count);
+
+                Console.ReadKey();
+            }
+            connection.Close();
+            Console.ReadLine();
+        }
+        static void combinations(List<string> list, List<string> fin)
+        {
+            double count = Math.Pow(2, list.Count);
+            for (int i = 1; i <= count - 1; i++)
+            {
+                string str = Convert.ToString(i, 2).PadLeft(list.Count, '0');
+                //Console.WriteLine(str);
+                string comb = "";
+                for (int j = 0; j < str.Length; j++)
+                {
+
+                    if (str[j] == '1')
+                    {
+                        comb = comb + list[j] + "||";
+                    }
+
+                }
+                //Console.WriteLine(comb);
+                fin.Add(comb);
+            }
+        }
+    }
+}
